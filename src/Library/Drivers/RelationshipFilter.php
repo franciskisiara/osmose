8628ffb1bc7502fb5bfb46559b4309ca55e9a9bb
Francis Kisiara <fkisiara@sendyit.com>
2019-07-03 20:39:06 +0300

    Added relationship filter driver

--- src/Library/Drivers/RelationshipFilter.php
+++ src/Library/Drivers/RelationshipFilter.php
@@ -2,11 +2,25 @@
 namespace Agog\Osmose\Library\Drivers;
 
 use Agog\Osmose\Library\Services\Contracts\OsmoseDriverInterface;
+use Agog\Osmose\Library\Decorators\OsmoseDriver;
 
-class RelationshipFilter implements OsmoseDriverInterface
+class RelationshipFilter extends OsmoseDriver implements OsmoseDriverInterface
 {
     public function filtrate ($builder)
     {
-        
+        if ($filter = request()->get($this->filter)) {
+
+            $rules = explode(":", $this->rule)[1];
+
+            $load = explode(",", $rules);
+
+            return $builder->whereHas($load[0], function ($query) use($load, $filter){
+
+                $query->where($load[1], $filter);
+
+            });
+        }
+
+        return $builder;
     }
 }
