edb2667838d9572481f270af0df278027f43e149
Lexx YungCarter <spencermainaa@gmail.com>
2021-11-17 02:33:55 +0300

    $defaultLimit
    Added `setDefaults()` method

--- src/Library/Services/Traits/OsmoseDatesTrait.php
+++ src/Library/Services/Traits/OsmoseDatesTrait.php
@@ -14,7 +14,12 @@ trait OsmoseDatesTrait
         return config("osmose.ranges.".request($range));
     }
 
-    public function limitTimespan ($limits)
+    /**
+     * get the timeline limits
+     * @param array $timeLimits
+     * @param string $defaultLimit [d,m,w,y]
+     */
+    public function limitTimespan ($limits, $defaultLimit = 'd')
     {
         $fromDate = request($limits['from'])
             ? Carbon::parse(request($limits['from']))->toDateTimeString()
@@ -24,10 +29,63 @@ trait OsmoseDatesTrait
             ? Carbon::parse(request($limits['to']))->endOfDay()->toDateTimeString()
             : today()->endOfDay()->toDateTimeString();
 
-        if (!$fromDate) return null;
+        if (! $fromDate) {
+
+            if(! $defaultLimit)
+                return null;
+
+            $defaults = $this->setDefaults($defaultLimit);
+            $fromDate = $defaults[0];
+            $toDate = $defaults[1];
+        }
 
         return [
             $fromDate, $toDate
         ];
+    }
+    
+    
+    /**
+     * @param string $limit determines whether blind date is set in a month, week or day
+     * @return array
+     * Sets the defaults based on carbon's current date
+     */
+    public function setDefaults($limit)
+    {
+        if(! in_array($limit, ['d', 'w', 'm', 'y'])) {
+            abort(500, 'Unknown Date Filter!');
+        }
+
+        if($limit == "d")
+        {
+            return [
+                now()->startOfDay(), 
+                now()->endOfDay(),
+            ];
+        }
+
+        if($limit == "w")
+        {
+            return [
+                now()->startOfWeek(), 
+                now()->endOfWeek(),
+            ];
+        }
+
+        if($limit == "m")
+        {
+            return [
+                now()->startOfMonth(), 
+                now()->endOfMonth(),
+            ];
+        }
+
+        if($limit == "y")
+        {
+            return [
+                now()->startOfYear(), 
+                now()->endOfYear(),
+            ];
+        }
     }
 }
