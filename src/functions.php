d55c1c891390ae618bc304e88d17277bbfc5bf16
Frank <franciskisiara@gmail.com>
2024-10-23 18:21:59 +0300

    started working on version 3

--- src/functions.php
+++ src/functions.php
@@ -5,17 +5,17 @@ if (!function_exists('osmose'))
     /**
      * Sieves an eloquent model against a specified osmose filter
      *
-     * @param  \Agog\Osmose\Library\OsmoseFilter $filter
-     * @param  \Illuminate\Database\Eloquent\Model $model optional
-     * @return \Illuminate\Http\Response
+     * @param  string  $filter
+     * @param  \Illuminate\Database\Eloquent\Model  $model optional
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    function osmose ($filter, $model = null)
+    function osmose ($filter, $model = null): \Illuminate\Database\Eloquent\Builder
     {
         if (is_null($model))
         {
             $namespace = config('osmose.namespace');
 
-            $namespace = $namespace ? trim($namespace, "/") : "App";
+            $namespace = $namespace ? trim($namespace, "/") : "App\\Models";
 
             $filterName = substr($filter, strrpos($filter, "\\") + 1);
 
